cba3d9e4a48b988a694d79860740379510943eaf
Nikhil Kudupudi <nikhilkudupudi@gmail.com>
2022-11-04 22:38:22 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React, { Suspense} from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter , Routes, Route } from 'react-router-dom';
 import User from './components/Index/User';
 import Login from './components/Index/Login';
 import ProductDetails from './components/Home/ProductDetails';
@@ -9,18 +9,19 @@ import UpdateProduct from './components/Home/UpdateProduct';
 function App(props) {
   return (
     <>
-    
-  <Router>
-  <Suspense fallback={<div>Loading...</div>}>
+   <BrowserRouter basename="/Productmanagement
+">
+
     <Routes>
-      <Route  path="/" element= {<User/>} />
+      <Route exact path="/Productmanagement
+" element= {<User/>} />
       <Route  path="/Login" element= {<Login/>} />
       <Route path="/home" element={<Home/>} />
       <Route path="/ProductDetails" element={<ProductDetails/>}/>
       <Route path="/updateproduct" element={<UpdateProduct/>}/>
     </Routes>
-  </Suspense>
-</Router>
+
+</BrowserRouter> 
 
 </>
   );
